0e50b9e73586882c5db5a724322c6e234dbde14d
Marvin <ddesbonnes@hotmail.fr>
2019-05-23 14:27:11 +0200

    version corrigée

--- js/marvin.js
+++ js/marvin.js
@@ -7,32 +7,31 @@ $(document).ready(function () {
     $("#bodyCercle").keydown(function (event) {
         switch (event.which) {
             case 40 :
-                top = top + 150;
-                $("#cerlce").animate({marginTop: top + "px"},500,function (){});
+                top = "+=100px";
+                $("#cerlce").animate({marginTop: top},500,function (){});
                 console.log("touche du bas");
                 break;
 
             case 38 :
-                top = top - 150;
-                $("#cerlce").animate({marginTop: top + "px"},500,function (){});
+                top = "-=100px";
+                $("#cerlce").animate({marginTop: top},500,function (){});
                 console.log("touche du haut");
                 break;
 
             case 39:
-                left = left + 150;
-                $("#cerlce").animate({marginLeft: left + "px"},500,function (){});
+                left = "+=100px";
+                $("#cerlce").animate({marginLeft: left},500,function (){});
                 console.log("touche de droite");
                 break;
 
             case 37:
-                left = left - 150;
-                $("#cerlce").animate({marginLeft: left + "px"},500,function (){});
+                left = "-=100px";
+                $("#cerlce").animate({marginLeft: left},500,function (){});
                 console.log("touche de gauche");
                 break;
 
+            default:return; // exit this handler for other keys
         }
-            
-
+        event.preventDefault(); // prevent the default action (scroll / move caret)
     });
-
 });
